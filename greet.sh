d331e8455ebf2446509bf41a6c08a9403f973d34
Keyur Patel <143982764+kp3073@users.noreply.github.com>
2024-02-06 10:28:41 -0500

    Create greet.sh

---
+++ greet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Simple Shell Script
+
+# Prompt the user for their name
+echo "Hello! What's your name?"
+
+# Read user input and store it in the variable 'name'
+read name
+
+# Display a personalized greeting
+echo "Nice to meet you, $name!"
